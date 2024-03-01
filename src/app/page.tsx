ec4e04db16a7df1ef14583c0a973dc0bc1ef5577
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2024-03-01 11:53:38 +0000

    feat: Src

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,36 @@
+"use client";
+
 import { nanoid } from "nanoid";
+import { useState } from "react";
+
+const links = [
+  "/r3f-cursors/",
+  "/r3f-cursor-lines/",
+  "/r3f-starter-kit/",
+  "/html-cursor-lines/",
+];
 
 export default function Home() {
-  return <a href={"/room/" + nanoid()}>Join a random room</a>;
+  const [random, setRandom] = useState(false);
+  const suffix = random ? nanoid() : "default-room";
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", gap: 6, padding: 20 }}
+    >
+      <h1 style={{ margin: "4px 0" }}>Enter rooms</h1>
+      {links.map((link) => (
+        <a key={link} href={link + suffix}>
+          {link + suffix}
+        </a>
+      ))}
+      <label style={{ userSelect: "none" }}>
+        Join random room?{" "}
+        <input
+          type="checkbox"
+          checked={random}
+          onChange={(e) => setRandom(e.target.checked)}
+        />
+      </label>
+    </div>
+  );
 }
--- src/app/r3f-starter-kit/[roomId]/CollaborativeApp.tsx
+++ src/app/r3f-starter-kit/[roomId]/CollaborativeApp.tsx
@@ -2,50 +2,81 @@
 
 import { Canvas } from "@react-three/fiber";
 import { useUpdateMyPresence } from "@/liveblocks.config";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Cursors } from "./Cursors";
 import { Html } from "@react-three/drei";
 
 export function CollaborativeApp() {
   const updateMyPresence = useUpdateMyPresence();
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [iframeSrc, setIframeSrc] = useState(
+    "https://nextjs-starter-kit.liveblocks.app/text/0LLet8_xRjahcLWJBxDfA"
+  );
 
   return (
-    <Canvas
-      ref={canvasRef}
-      style={{ width: "100vw", height: "100vh", background: "coral" }}
-      camera={{ position: [1, 0.5, 4.5] }}
-    >
-      <group
-        onPointerMove={(event) => {
-          const [x, y] = event.point.toArray();
-          updateMyPresence({
-            cursor: { x, y },
-          });
+    <>
+      <Canvas
+        ref={canvasRef}
+        style={{
+          width: "100vw",
+          height: "100vh",
+          background: "black",
+        }}
+        camera={{ position: [1, 0.5, 4.5] }}
+      >
+        <group>
+          <ambientLight intensity={0.1} />
+          <directionalLight color="#fff" position={[1, 2, 3]} />
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 10, 10]} />
+          <Html
+            position={[8, 2, -20]}
+            transform
+            occlude
+            onPointerMove={(event) => {
+              const [x, y] = event.point.toArray();
+              updateMyPresence({
+                cursor: { x, y },
+              });
+            }}
+          >
+            <Tablet src={iframeSrc} />
+          </Html>
+          <Html
+            position={[-18.5, -8, -16]}
+            rotation={[0, 0.6, 0]}
+            transform
+            occlude
+          >
+            <Tablet src={iframeSrc} />
+          </Html>
+          <Cursors />
+        </group>
+      </Canvas>
+      <label
+        style={{
+          position: "absolute",
+          top: 15,
+          left: 15,
+          color: "#fff",
+          display: "flex",
+          flexDirection: "column",
+          gap: 8,
         }}
       >
-        <ambientLight intensity={0.1} />
-        <directionalLight color="#fff" position={[1, 2, 3]} />
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
-        <Html position={[8, 2, -20]} transform occlude>
-          <Tablet />
-        </Html>
-        <Html
-          position={[-18.5, -8, -16]}
-          rotation={[0, 0.6, 0]}
-          transform
-          occlude
-        >
-          <Tablet />
-        </Html>
-        <Cursors />
-      </group>
-    </Canvas>
+        Website URL
+        <input
+          type="text"
+          value={iframeSrc}
+          onChange={(e) => setIframeSrc(e.target.value)}
+          style={{ padding: 4 }}
+        />
+      </label>
+    </>
   );
 }
 
-function Tablet() {
+function Tablet({ src }: { src: string }) {
   return (
     <div
       style={{
@@ -57,7 +88,7 @@ function Tablet() {
       }}
     >
       <iframe
-        src="https://nextjs-starter-kit.liveblocks.app/text/0LLet8_xRjahcLWJBxDfA"
+        src={src}
         title="Liveblocks"
         width="1000"
         height="700"
